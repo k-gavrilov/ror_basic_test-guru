81584128fe8500ecf5574a3a9d4c00dec970c174
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-11 10:48:45 +0300

    Revert "Add information about project and author"
    This reverts commit 432c921abd356ec111c905d95da7852c593bf58d.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'about/', to: 'about#index'
-  get 'about/author', to: 'about#author'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
