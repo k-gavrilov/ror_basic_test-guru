f7093a599ec15e869ce306f5f16cf8cddaa0783e
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-13 21:49:25 +0300

    Generate Question

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_184341) do
+ActiveRecord::Schema.define(version: 2018_09_13_184743) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "body"
+    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
