a19259bf6c496f06a35a7fa3d9a9e72d3e868638
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-13 21:45:58 +0300

    Generate Test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_183823) do
+ActiveRecord::Schema.define(version: 2018_09_13_184341) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title"
+    t.integer "level"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
