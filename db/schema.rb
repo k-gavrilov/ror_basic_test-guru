3cebbe686d2c29771baad5b1d06367f5138d2a05
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-14 09:11:25 +0300

    Add title null constraint to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_185819) do
+ActiveRecord::Schema.define(version: 2018_09_14_060810) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_185819) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
