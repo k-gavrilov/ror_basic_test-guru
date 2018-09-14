e1414c7e4d5493db3155c1def56363287a3169fa
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-14 09:35:16 +0300

    Add correct default value to answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_062053) do
+ActiveRecord::Schema.define(version: 2018_09_14_063136) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
-    t.boolean "correct"
+    t.boolean "correct", default: false
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
