62db765af4ed3e99663584ee09159ab19691ee03
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-14 09:22:27 +0300

    Add body null contraint to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_061554) do
+ActiveRecord::Schema.define(version: 2018_09_14_061901) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2018_09_14_061554) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
