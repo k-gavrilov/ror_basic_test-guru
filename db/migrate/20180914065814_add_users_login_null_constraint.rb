f57e084261bbd6e3438af31049b9afbeb2e04877
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-15 10:30:19 +0300

    Replace multiple migrations with two dedicated ones

--- db/migrate/20180914065814_add_users_login_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersLoginNullConstraint < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null(:users, :login, false)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_065814) do
+ActiveRecord::Schema.define(version: 2018_09_15_072301) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
