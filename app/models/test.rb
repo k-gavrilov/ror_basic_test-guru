bc09e9f82050ac3dc69f475ef42a875b2760b06a
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-18 09:17:32 +0300

    Find test names by category name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.find_test_names_by_category(category_title)
+    category_ids = Category.where(title: category_title).collect(&:id)
+    where("category_id IN (?)", category_ids).order(title: :desc).collect(&:title)
+  end
 end
