962418a7bec1e66d5d17c5ad219127cfc076c403
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-18 14:55:54 +0300

    Refactor titles_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
-  def self.find_test_names_by_category(category_title)
-    category_ids = Category.where(title: category_title).collect(&:id)
-    where("category_id IN (?)", category_ids).order(title: :desc).collect(&:title)
+  def self.titles_by_category(category_title)
+    joins("INNER JOIN categories ON tests.category_id = categories.id")
+      .where("categories.title = ?", category_title).pluck(:title).sort
   end
 end
