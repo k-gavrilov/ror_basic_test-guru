af341fe4fb7de247f9302ed79b75d0b7df60308f
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-18 08:57:24 +0300

    Find test by level for specified user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def test_by_level(level)
+    test_result_ids = TestResult.where(user_id: id).collect(&:test_id)
+    Test.where("id IN (?) AND level = ?", test_result_ids, level)
+  end
 end
