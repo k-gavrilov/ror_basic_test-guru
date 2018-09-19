274613a32a6de3667721c5374eed4803712458d6
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-19 21:20:11 +0300

    Simplify titles_by_category method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
     Test.joins("INNER JOIN test_results ON tests.id = test_results.test_id")
-        .where("user_id = ? AND level = ?", id, level)
-        .select('tests.id, tests.title, tests.level, tests.category_id')
+        .where(level: level, test_results: { user_id: id })
   end
 end
