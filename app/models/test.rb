1e24e49a610ba9f851e5329184193af7be1e7abd
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-22 19:23:59 +0300

    Implement Test_Category assosiation

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
   def self.titles_by_category(category_title)
     joins("INNER JOIN categories ON tests.category_id = categories.id")
       .where("categories.title = ?", category_title).order(:title).pluck(:title)
