8077f04b0db66933a24a08d434bab276396a96e7
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-27 21:07:16 +0300

    Remove invalid association

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,6 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
-  belongs_to :user, -> { where role: "Author" }, dependent: :destroy
 
   def self.titles_by_category(category_title)
     Category.find_by(title: category_title).tests.order(:title).pluck(:title)
