b0fae66b89d7c95df7ade3dbfe73ebe0205e9c35
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-09-28 23:16:05 +0300

    Make test_passings table instead of tests_users join table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :test_passings
+  has_many :tests, through: :test_passings
   has_many :works, class_name: "Test", foreign_key: :author_id
 
   def tests_by_level(level)
