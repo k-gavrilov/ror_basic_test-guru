556f2d457dc1df9f187862bf644c85892c2e20b6
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-10-03 19:53:01 +0300

    Make author-created_tests association

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: "User"
   has_many :questions, dependent: :destroy
   has_many :test_passings, dependent: :destroy
   has_many :users, through: :tests_passings
