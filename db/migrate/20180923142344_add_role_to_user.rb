6a5f1efdd90b90029887429c19c1fe9c556f113f
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-10-03 19:53:01 +0300

    Move entire migration to the change method.

--- db/migrate/20180923142344_add_role_to_user.rb
+++ db/migrate/20180923142344_add_role_to_user.rb
@@ -1,9 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration[5.2]
-  def up
+  def change
     add_column :users, :role, :string, null: false
-  end
-
-  def down
-    remove_column :users, :role
   end
 end
