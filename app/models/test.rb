1446c221f0fbb26066cb80fa3da81687790f7e44
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-10-07 22:10:26 +0300

    Pair title-level should be unique.

--- app/models/test.rb
+++ app/models/test.rb
@@ -15,4 +15,5 @@ class Test < ApplicationRecord
 
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level }
 end
